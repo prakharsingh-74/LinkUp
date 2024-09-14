3a7e8e5aae0b6de5fdb57b491da4eccf83024644
Prakhar Singh <prakhar22153054@akgec.ac.in>
2024-09-14 20:06:02 +0530

    Refactor RootLayout component to remove unnecessary elements and update metadata description

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,33 @@
+'use client'
+import { sidebarLinks } from '@/app/constants'
+import Link from 'next/link';
+import { usePathname } from 'next/navigation'
+import { cn } from '@/lib/utils';
 import React from 'react'
 
 const Sidebar = () => {
+  const pathname = usePathname();
   return (
-    <div>Sidebar</div>
-  )
+    <section className="sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
+      <div className="flex flex-1 flex-col gap-6">
+        {sidebarLinks.map((link) => {
+          const isActive = pathname === link.route || pathname.startsWith(link.route);
+          
+          return (
+            <Link
+              href={link.route} // Fixed from Link.route to link.route
+              key={link.label}
+              className={cn('flex gap-4 items-center p-4 rounded-lg justify-start', {
+                'bg-dark-2': isActive,
+              })}
+            >
+              {link.label}
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
