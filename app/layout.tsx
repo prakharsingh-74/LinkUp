b7844a64e2e3037c3c03e543e46b003c69055645
Prakhar Singh <prakhar22153054@akgec.ac.in>
2024-09-15 17:19:37 +0530

    Refactor CSS to add utility classes for flex center and flex between

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,9 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <ClerkProvider>
       <body className={`${inter.className} bg-dark-2`}>
         {children}
       </body>
+      </ClerkProvider>
     </html>
   );
 }
