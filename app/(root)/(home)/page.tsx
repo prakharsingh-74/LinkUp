d6ee19e8178d65efbd8c75cf75dc1b8dfc7d8e78
Prakhar Singh <prakhar22153054@akgec.ac.in>
2024-09-16 19:50:48 +0530

    Refactor home page to display upcoming meeting time and date

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React from 'react';
+
 
 const Home = () => {
-  return (
-    <section className="flex size-full flex-col gap-10 text-white">
-       <h1 className="text-3xl font-bold"></h1>
-       Home
-    </section>
-  )
-}
+  const now = new Date();
 
-export default Home
+  const time = now.toLocaleTimeString('en-IN', { hour: '2-digit', minute: '2-digit' });
+  const date = (new Intl.DateTimeFormat('en-IN', { dateStyle: 'full' })).format(now);
+
+  return (
+    <section className="flex size-full flex-col gap-5 text-white">
+      <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
+        <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
+          <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal">
+            Upcoming Meeting at: 12:30 PM
+          </h2>
+          <div className="flex flex-col gap-2">
+            <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
+            <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
+          </div>
+        </div>
+      </div>
+
+      
+    </section>
+  );
+};
+
+export default Home;
