507164324cfd7d83c076f7eb1023bdd959a52436
Prakhar Singh <prakhar22153054@akgec.ac.in>
2024-09-21 01:47:20 +0530

    Add "@radix-ui/react-toast" and "@stream-io/node-sdk" dependencies

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -3,14 +3,60 @@ import React, { useState } from 'react'
 import HomeCard from './HomeCard'
 import { useRouter } from 'next/navigation'
 import MeetingModal from './MeetingModal'
+import { useUser } from '@clerk/nextjs'
+import { DefaultReactionsMenu, useStreamVideoClient, Call } from '@stream-io/video-react-sdk'
+import { useToast } from "@/hooks/use-toast"
 
 const MeetingTypeList = () => {
   const router = useRouter();
   const [meetingState, setMeetingState] =
     useState<'isSchedulemeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined>()
-    
-  const createMeeting = () => {
-    console.log('Instant Meeting Created');
+  const { user } = useUser();
+  const client = useStreamVideoClient(); 
+  const[values, setValues] = useState({
+    dateTime: new Date(),
+    Description: '',
+    link: ''
+  })
+  
+  const [callDetails, setCallDetails] = useState<Call>()
+  const { toast } = useToast()
+
+  const createMeeting = async () => {
+    if(!client || !user) return;
+
+    try{
+      if (!values.dateTime) {
+        toast({ title: 'Please select a date and time' });
+        return;
+      }
+
+      const id = crypto.randomUUID();
+      const call = client.call('default', id);
+
+      if(!call) throw new Error("Failed to create a call");
+      const startsAt = values.dateTime.toISOString() || new Date(Date.now()).toISOString();
+      const description = values.Description || 'Instant meeting';
+
+      await call.getOrCreate({
+        data: {
+          starts_at: startsAt,
+          custom: {
+            description,
+          },
+        },
+      })
+      setCallDetails(call);
+      if (!values.Description) {
+        router.push(`/meeting/${call.id}`);
+      }
+      
+      toast({title: "Meeting created"})
+    } catch(error){
+     console.log(error);
+    }toast({
+      title: "Failed to create meeting",
+    })
   }
 
   return (
