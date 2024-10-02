9326ad5c0c6e9c143f190f98dec46ecf9bcb9f88
Prakhar Singh <prakhar22153054@akgec.ac.in>
2024-10-02 10:52:47 +0530

    Refactor layout and add metadata to components

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/toaster"
+import "@stream-io/video-react-sdk/dist/css/styles.css";
+import 'react-datepicker/dist/react-datepicker.css'
 
 const inter = Inter({ subsets: ["latin"] });
 import '@stream-io/video-react-sdk/dist/css/styles.css';
--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -7,7 +7,9 @@ import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk';
 import { useUser } from '@clerk/nextjs';
 import Loader from './Loader';
 import { useToast } from "@/hooks/use-toast"
-
+import { Textarea } from './ui/textarea';
+import ReactDatePicker from 'react-datepicker';
+import { Input } from './ui/input';
 
 const initialValues = {
   dateTime: new Date(),
@@ -111,11 +113,27 @@ const MeetingTypeList = () => {
             <label className="text-base font-normal leading-[22.4px] text-sky-2">
               Add a description
             </label>
+            <Textarea
+              className="border-none bg-dark-3 focus-visible:ring-0 focus-visible:ring-offset-0"
+              onChange={(e) =>
+                setValues({ ...values, description: e.target.value })
+              }
+            />
           </div>
           <div className="flex w-full flex-col gap-2.5">
             <label className="text-base font-normal leading-[22.4px] text-sky-2">
               Select Date and Time
             </label>
+            <ReactDatePicker
+              selected={values.dateTime}
+              onChange={(date) => setValues({ ...values, dateTime: date! })}
+              showTimeSelect
+              timeFormat="HH:mm"
+              timeIntervals={15}
+              timeCaption="time"
+              dateFormat="MMMM d, yyyy h:mm aa"
+              className="w-full rounded bg-dark-3 p-2 focus:outline-none"
+            />
           </div>
         </MeetingModal>
       ) : (
@@ -127,7 +145,7 @@ const MeetingTypeList = () => {
             navigator.clipboard.writeText(meetingLink);
             toast({ title: 'Link Copied' });
           }}
-          image="/icons/checked.svg"
+          image={'/icons/checked.svg'}
           buttonIcon="/icons/copy.svg"
           className="text-center"
           buttonText="Copy Meeting Link"
@@ -142,7 +160,11 @@ const MeetingTypeList = () => {
         buttonText="Join Meeting"
         handleClick={() => router.push(values.link)}
       >
-
+        <Input
+          placeholder="Meeting link"
+          onChange={(e) => setValues({ ...values, link: e.target.value })}
+          className="border-none bg-dark-3 focus-visible:ring-0 focus-visible:ring-offset-0"
+        />
       </MeetingModal>
 
       <MeetingModal
